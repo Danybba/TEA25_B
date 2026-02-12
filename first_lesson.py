3f40bc7ab395ce74b5536383175ac09b91fdaef2
Daniel Schäftner <daniel.schaeftner@schaeftner-engineering.com>
2026-02-12 18:38:35 +0100

    first lesson

---
+++ first_lesson.py
@@ -0,0 +1,31 @@
+# Author: Daniel Schäftner 
+# Date: 2024-06-01
+
+########################################################
+### Programmieren lernen mit Python - Erste Schritte ###
+########################################################
+
+""" Das hier ist unser erstes Python-Programm. Es soll "Hello World" ausgeben. """
+
+# print("Hello World")
+# print("This is my first Python code")
+# print("I am learning Python")
+
+# snake_case_variable = 25
+# CamelCaseVariable = 25
+
+# var_1 = 25
+# var_2 = 25 
+
+variable = 25
+# variable = 30.5
+# variable = "Text"
+
+vorname = "Daniel "
+nachname = "Schäftner"
+
+gesamter_name = vorname + nachname
+
+summe = variable + variable
+
+print(summe)
