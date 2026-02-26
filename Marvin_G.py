18fdc1d9405ec66d016b3424a36d4ab4ad6c7337
Marvin2264 <Marvingorbach500@gmail.com>
2026-02-26 16:44:29 +0100

    HR added

--- Marvin_G.py
+++ Marvin_G.py
@@ -1,1 +1,12 @@
-print("Marvin_G")
+# Author: Marvin Gorbach
+# Date: 2026-02-12
+
+####################################
+# Programmieren lernen mit Python #
+##################################
+
+heart_rate = 77
+too_low = heart_rate < 60
+too_high = heart_rate > 100
+print("Heart rate low", too_low)
+print("heart rate high", too_high)
